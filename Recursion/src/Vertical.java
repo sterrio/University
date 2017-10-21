2d73c0e604aabe90efeda3ce52ffaf1bbd893158
jarretterrio <jarretterrio@live.com>
2017-10-21 19:14:51 -0300

    Added verticals

---
+++ Recursion/src/Vertical.java
@@ -0,0 +1,16 @@
+// Algorithm Complexity - Vertical October 19th 2017 - Stephen Terrio B00755443
+import java.util.Scanner;
+
+public class Vertical {
+	public static void main(String [] args){
+		
+		Scanner sc = new Scanner (System.in);
+		int converter = sc.nextInt();
+		
+		vertical(converter);
+		
+	}
+	public static void vertical(int x){
+		
+	}
+}
