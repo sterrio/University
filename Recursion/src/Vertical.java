ed4795b996acfa41c3ea37b968527df319e8573b
jarretterrio <jarretterrio@live.com>
2017-10-22 20:39:43 -0300

    Finished Vertical int converter

--- Recursion/src/Vertical.java
+++ Recursion/src/Vertical.java
@@ -7,10 +7,25 @@ public class Vertical {
 		Scanner sc = new Scanner (System.in);
 		int converter = sc.nextInt();
 		
+		
 		vertical(converter);
 		
 	}
 	public static void vertical(int x){
 		
+		// Changing the integer to a string and getting the first character
+		String temp = Integer.toString(x);
+		System.out.println(temp.charAt(0));
+		// Setting a variable to a substring of every number after the first
+		String temp_x = temp.substring(1);
+		
+		// if there's more numbers.. run the following code
+		if (temp_x.equals("")){return;}
+		
+		else{
+			// Change the substring to an integer and run the function again
+		int new_x = Integer.parseInt(temp_x);
+		vertical(new_x);
+		}
 	}
 }
