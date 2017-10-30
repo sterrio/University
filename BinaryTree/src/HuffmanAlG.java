ea078d5fc1ae820e9c8d16496ac21248385d7956
jarretterrio <jarretterrio@live.com>
2017-10-30 20:28:18 -0300

    Added the Huffman tree ALG

---
+++ BinaryTree/src/HuffmanAlG.java
@@ -0,0 +1,24 @@
+// Assignment 2 - Binary Tree, October 30th - Stephen Terrio, B00755443
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class HuffmanAlG {
+	public static void main (String [] agrs)throws IOException{
+		
+		// Getting the file location
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the location of your text file: ");
+		String filename = sc.nextLine();
+		
+		// Reading the file
+		File text = new File(filename);
+		Scanner file = new Scanner(text);
+		
+		StringTokenizer token;
+		
+		while (file.hasNext()){
+			
+		}
+	}
+}
