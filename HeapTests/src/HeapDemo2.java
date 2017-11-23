b8f5f923eba3ac088825b136fd78116b8b7f4c7e
jarretterrio <jarretterrio@live.com>
2017-11-23 13:18:20 -0400

    Added a demo file

--- HeapTests/src/HeapDemo2.java
+++ HeapTests/src/HeapDemo2.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
 
+// Assignment 4 Heap sort - Stephen Terrio B00755443 
 public class HeapDemo2 {
-
+	public static void main(String [] args)throws IOException{
+		
+		Heap<String> myHeap = new Heap<String>();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the location of you text file: ");
+		String location = sc.nextLine();
+		
+		File file = new File(location);
+		Scanner reader = new Scanner(file);
+		PrintWriter writer = new PrintWriter("Output.txt");
+		
+		while (reader.hasNext()){
+			myHeap.add(reader.next());
+		}
+		
+		while (!myHeap.isEmpty()){
+			String temp = myHeap.deleteMax();
+			writer.println(temp);
+		}
+		writer.close();
+	}
 }
+///Users/jarretterrio/Desktop/Unsorted.txt
