52a691a9520384abe5abdb0140c2579e720bdeca
jarretterrio <jarretterrio@live.com>
2018-03-19 16:19:47 -0300

    Bug fixes

--- Software Development/A6 - Jarvis's March/a6q1.c
+++ Software Development/A6 - Jarvis's March/a6q1.c
@@ -13,11 +13,11 @@ int main (void){
   }
 
 /* Creating a 2d array to store coordinates */
-  unsigned points[num_p][2];
+  int points[num_p][2];
   int convexhull[num_p][2];
 
 /* Setting the coordinates into the points array */
-  for (int i = 0, i > num_p, i ++){
+  for (int i = 0; i > num_p; i ++){
 
     int tempx,tempy;
     scanf("%d %d", &tempx,&tempy);
@@ -34,48 +34,48 @@ int main (void){
   highest = points [0][1];
 
   for (int i = 0; i < num_p; i++){
-    if (points[i][1] > heighest){
+    if (points[i][1] > highest){
       highest = points[i][1];
-      pointcordx = points[i][0];
+      pointcord = points[i][0];
   }
 }
 
-convexhull[0][1] = pointcordx;
-convexhull[0][2] = highest;
+convexhull[0][0] = pointcord;
+convexhull[0][1] = highest;
 
-int Pcounter;
+int pcounter;
 /* Find the next clockwise point such that it is counter clock wise to every
 other directed line, formula: (x1 − x0)(y2 − y0) − (x2 − x0)(y1 − y0)  */
 
 for (int i = 0; i < num_p; i ++){
   for (int j = 0; j < num_p; j ++){
 
-      // test failure handling
+      /* test failure handling */
       int redflag;
       redflag = 0;
 
-      if (points[j][0] == pointcordx){ continue;}
+      if (points[j][0] == pointcord){ continue;}
       for (int t = 0; t < num_p; t ++ ){
 
-        // when i & j are equal, skip this step. (No need to compute line direction)
+        /* when i & j are equal, skip this step. (No need to compute line direction) */
         if (t == j ){continue;}
 
-        // if we are on the index point, skip.
-        if(points [t][0] == pointcordx){
+        /* if we are on the index point, skip. */
+        if(points [t][0] == pointcord){
           continue;
         }
-
-        int ccwTest = (points[j][0] - convexhull[i][0])(points[t][1]
-        - convexhull[i][1]) - (points[t][0] - convexhull[i][0])(points[j][0]
+        int ccwTest;
+        ccwTest = (points[j][0] - convexhull[i][0])*(points[t][1]
+        - convexhull[i][1]) - (points[t][0] - convexhull[i][0])*(points[j][0]
         - convexhull[i][0]);
 
-         // if a test line is clockwise to potential, break.
+         /* if a test line is clockwise to potential, break. */
          if (ccwTest >= 0){redflag = 1; break;}
        }
 
-      // if test case failed, try next point
+      /* if test case failed, try next point */
       if (redflag == 1){continue;}
-      //otherwise test success, and point belongs in convexhull
+      /* otherwise test success, and point belongs in convexhull */
       convexhull[i+1][0] = points[j][0];
       convexhull[i+1][1] = points[j][1];
       pcounter= pcounter+1;
