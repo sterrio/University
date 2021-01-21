2d13ef79713f2ea02d92ef44e7171ada05a72b66
jarretterrio <jarretterrio@live.com>
2021-01-21 02:19:12 -0400

    small additions

---
+++ Systems Programming/triangle trig/triangle.h
@@ -0,0 +1,12 @@
+/* Stephen Terrio B00755443 */
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+double findOppositePyth(double, double, double, double);
+double findAdjacentPyth(double, double, double, double);
+double findHypotenusePyth(double, double, double, double);
+double findOppositeTrig(double, double, double, double);
+double findAdjacentTrig(double, double, double, double);
+double findHypotenuseTrig(double, double, double, double);
+
+#endif
