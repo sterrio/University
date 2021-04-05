e0b262b4e5471ada4931c53e47401f2319cceb34
jarretterrio <jarretterrio@live.com>
2021-04-04 21:38:19 -0300

    first part of project A

---
+++ Python with Science Applicaitons/Project A/projectA.py
@@ -0,0 +1,49 @@
+# Stephen Terrio, B00755443
+# Project A - Page Rank Simulation:
+
+# Packages -
+import numpy as np
+import math
+
+# Change this to the corresponding file name  -
+file = 'tiny.txt'
+
+# Taking defining file input function
+def adjMatrixFromFile(filename):
+    # Reading file and getting the size of matrix [N]x[N]
+    dataXY = open(filename,'r')
+    lines = dataXY.readlines()
+    matrixN = int(lines[0][0])
+    nodeMatrix = []
+
+    # Using that size to init the Adj Matrix
+    adjArray = np.zeros((matrixN, matrixN))
+    # We now have a string array of inputs, looping through each line for adj calcs
+    for line in lines:
+        currentLine = line.strip()
+        index = currentLine.split(" ")
+        # Accounting for first line of N declaration
+        if(len(line) > 4):
+            # Loop through each value in lines
+            for i in range(len(line)):
+                # If it contains a adjacency value, add to matrix, else skip
+                if(line[i] == ' ' or line[i] == '\n'):
+                     i += 1
+                else:
+                    # Appending to a new node list
+                    node = int(line[i])
+                    nodeMatrix.append(node)
+
+    # Looping through node list and adding nodes to adj Matrix
+    for i in range(0, len(nodeMatrix), 2):
+        adjArray[nodeMatrix[i]][nodeMatrix[i+1]] += 1
+
+    dataXY.close()
+    return adjArray
+
+
+def outDegrees(adjacencyMatrix):
+
+
+# Function Calls -
+adjM = adjMatrixFromFile(file)
